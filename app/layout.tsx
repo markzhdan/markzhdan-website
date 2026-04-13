ae6b7643289c70d5630e514ffd51e6187d0b5128
Mark Zhdan <markzhdan@gmail.com>
2026-04-12 21:33:52 -0500

    feat: optimize CSS and image loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const bebasNeue = Bebas_Neue({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-bebas",
+  display: "block",
 });
 
 export const metadata: Metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { Prefetch } from "@/components/prefetch";
 export default function Homepage() {
   return (
     <main className="flex flex-col w-full max-w-100 px-2.5 gap-8">
+      <link rel="prefetch" as="image" href="/logos/blinkfire_logo.png" />
       <AboutMe />
       <Portfolio />
       <Socials />
